1d6d186bed9eda57d045325a6b0ca56ea8339226
Avazbek <YOUR_REAL_EMAIL@gmail.com>
2026-06-03 08:58:54 +0500

    mainga tashadim

---
+++ src/avaz.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function avaz() {
+  return (
+    <footer className="footer">
+  <div className="footer-left">
+    <p>
+      Raqamga qo'ng'iroq qiling <a href="tel:1174">1174</a>
+    </p>
+  </div>
+
+  <div className="footer-links">
+    <div className="column">
+      <a href="">Biz haqimizda</a>
+      <a href="">Ommaviy oferta</a>
+      <a href="">Maxfiylik siyosati</a>
+      <a href="">Halol sertifikati</a>
+      <a href="">Restoranlar</a>
+    </div>
+
+    <div className="column">
+      <a href="">Yetkazib beruvchi shakli</a>
+      <a href="">Bizning ish o'rinlarimiz</a>
+      <a href="">Allergenlar</a>
+    </div>
+    <div>
+<img className="logo" src="https://api.logobank.uz/media/logos_png/payme-01.png" alt="" />
+<img className="logo" src="https://api.logobank.uz/media/logos_png/Click-01_hjB080W.png" alt="" />
+<img className="logo" src="https://api.uzcard.uz/wp-content/uploads/2024/07/Uzcard_Logo_-3.png" alt="" />
+    </div>
+  </div>
+</footer>
+
+  )
+}
+
+export default avaz
